dfe4fa57573f8d271465e29c90523f2253d710c5
pautrik <>
2019-09-11 23:30:56 +0200

    Fixed broadcaster stuff

--- broadcaster/index.js
+++ broadcaster/index.js
@@ -1,15 +1,16 @@
 const isMeatballsToday = require('./isMeatballsToday');
-const { getSubs } = require('../db');
+const { init, getSubs } = require('../db');
 const { sendNotification } = require('../notification-manager');
 
 async function run() {
+    await init();
     const isMeatballs = await isMeatballsToday();
     if(!isMeatballs)
         return;
 
     const subs = await getSubs();
     for(let sub of subs) {
-        await sendNotification(sub);
+        await sendNotification(sub, { title: "BullePling", body: "There are meatballs at express today!!" });
     }
 }
 
